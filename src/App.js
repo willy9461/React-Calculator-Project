5ed1c05f399f154c845fb46745f827e38a497905
willy9461 <guillermogalarraga@gmail.com>
2023-03-03 13:47:01 -0300

    commit everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
+import { logDOM } from '@testing-library/react';
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton'
+import Pantalla from './componentes/Pantalla'
+import BotonClear from './componentes/BotonClear';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        src={freeCodeCampLogo} 
+        className='freecodecamp-logo' 
+        alt='Logo de FreeCodeCamp' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla />
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear>Clear</BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
